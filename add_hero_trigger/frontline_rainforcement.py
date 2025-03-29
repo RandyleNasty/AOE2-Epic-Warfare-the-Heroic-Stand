589cb0e43bafeacd87bb1c6e35ae1384a11a5141
RandyleNasty <zixiang.randy@gmail.com>
2025-03-29 11:29:16 +0100

    add gold mines to battle two sides

---
+++ add_hero_trigger/frontline_rainforcement.py
@@ -0,0 +1,44 @@
+"""
+DETECT
+ SPAWN
+
+ REACTIVATE TRIGGER
+
+
+ DETECT BUILD DESTORIED AND DISABLE ALL
+
+"""
+
+from AoE2ScenarioParser.datasets.terrains import TerrainId
+from AoE2ScenarioParser.datasets.buildings import BuildingInfo
+from AoE2ScenarioParser.datasets.units import UnitInfo
+from AoE2ScenarioParser.datasets.heroes import HeroInfo
+from AoE2ScenarioParser.datasets.techs import TechInfo
+from AoE2ScenarioParser.datasets.other import OtherInfo
+
+
+from AoE2ScenarioParser.datasets.players import PlayerId
+
+
+from AoE2ScenarioParser.datasets.trigger_lists import ObjectAttribute
+from AoE2ScenarioParser.datasets.trigger_lists import Operation
+
+
+from util_add_tent_spawn_ability import *
+
+
+from hero_abstract_class import *
+
+from robin_archer import *
+
+from dynamic_enviorment import *
+
+from center_sacred_parameters import *
+from hero_exploding_elephant import *
+
+# def create_frontline_reinforcement(source_trigger_manager, 
+#                                                                building_referece_id, 
+#                                                                building_unit_id,
+#                                                                moving_location_x, 
+#                                                                moving_location_y, 
+#                                                                source_player):
